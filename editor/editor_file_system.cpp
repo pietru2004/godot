24764f60b741ca66350f8c9b02a9b6548e007b86
pietru2004 <51274570+pietru2004@users.noreply.github.com>
2025-03-03 21:57:29 +0100

    Update editor_file_system.cpp

--- editor/editor_file_system.cpp
+++ editor/editor_file_system.cpp
@@ -2422,7 +2422,7 @@ void EditorFileSystem::update_files(const Vector<String> &p_script_paths) {
 			} else {
                 bool generate_uid_files = GLOBAL_GET("editor/resource/generate_uid_files");
 	     		bool generate_uid_files_for_addons = GLOBAL_GET("editor/resource/generate_uid_files_for_addons");
-				if (ResourceLoader::should_create_uid_file(file) && generate_uid_files && (generate_uid_files_for_addons || !path.begins_with("res://addons/"))) {
+				if (ResourceLoader::should_create_uid_file(file) && generate_uid_files && (generate_uid_files_for_addons || !file.begins_with("res://addons/"))) {
 					Ref<FileAccess> f = FileAccess::open(file + ".uid", FileAccess::WRITE);
 					if (f.is_valid()) {
 						const ResourceUID::ID id = ResourceUID::get_singleton()->create_id();
